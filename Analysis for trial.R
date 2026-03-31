9900651a4e598aca05b41bdc4631b1e9d019d31f
ntt12080@student.ubc.ca <ntt12080@student.ubc.ca>
2026-03-30 21:28:30 -0700

    Added chunk to edit Uniprot file and an example final version input for ermineJ

--- Analysis for trial.R
+++ Analysis for trial.R
@@ -322,7 +322,6 @@ mat_scaled <- t(scale(t(mat_top)))
 #not sure if I'll trust the z-score here, this is a rough estimate
 #Let's try both heatmap
 
-# lets try both heatmaps
 
 pheatmap(
   mat_scaled,
@@ -340,3 +339,150 @@ pheatmap(
   show_rownames = TRUE,
   main = "Top variable proteins"
 )
+
+##Did not do a differential expression analysis here, will do with real samples
+
+###PIck out top 20 proteins for GO Enrichment
+library(dplyr)
+
+# Start from proteins not detected in blank
+pg_noblank <- pg_classified %>%
+  filter(class == "High_confidence_bee") %>%
+  mutate(
+    log2_Crop = log2(Crop + 1),
+    log2_Hind = log2(Hind + 1),
+    log2_Mid  = log2(Mid + 1),
+    
+    Crop_vs_Mid  = log2_Crop - log2_Mid,
+    Crop_vs_Hind = log2_Crop - log2_Hind,
+    
+    Mid_vs_Crop  = log2_Mid - log2_Crop,
+    Mid_vs_Hind  = log2_Mid - log2_Hind,
+    
+    Hind_vs_Crop = log2_Hind - log2_Crop,
+    Hind_vs_Mid  = log2_Hind - log2_Mid
+  )
+
+# Top 20 Crop-enriched proteins
+top20_crop_enriched <- pg_noblank %>%
+  filter(!is.na(Crop)) %>%
+  mutate(crop_score = Crop_vs_Mid + Crop_vs_Hind) %>%
+  arrange(desc(crop_score)) %>%
+  select(
+    Protein.Group, Protein.Names, Genes, First.Protein.Description,
+    Crop, Hind, Mid, Crop_vs_Mid, Crop_vs_Hind, crop_score
+  ) %>%
+  head(20)
+
+# Top 20 Mid-enriched proteins
+top20_mid_enriched <- pg_noblank %>%
+  filter(!is.na(Mid)) %>%
+  mutate(mid_score = Mid_vs_Crop + Mid_vs_Hind) %>%
+  arrange(desc(mid_score)) %>%
+  select(
+    Protein.Group, Protein.Names, Genes, First.Protein.Description,
+    Crop, Hind, Mid, Mid_vs_Crop, Mid_vs_Hind, mid_score
+  ) %>%
+  head(20)
+
+# Top 20 Hind-enriched proteins
+top20_hind_enriched <- pg_noblank %>%
+  filter(!is.na(Hind)) %>%
+  mutate(hind_score = Hind_vs_Crop + Hind_vs_Mid) %>%
+  arrange(desc(hind_score)) %>%
+  select(
+    Protein.Group, Protein.Names, Genes, First.Protein.Description,
+    Crop, Hind, Mid, Hind_vs_Crop, Hind_vs_Mid, hind_score
+  ) %>%
+  head(20)
+
+# View them
+View(top20_crop_enriched)
+View(top20_mid_enriched)
+View(top20_hind_enriched)
+##Save them as files
+write.csv(top20_crop_enriched, "top20_crop_enriched.csv", row.names = FALSE)
+write.csv(top20_mid_enriched,  "top20_mid_enriched.csv", row.names = FALSE)
+write.csv(top20_hind_enriched, "top20_hind_enriched.csv", row.names = FALSE)
+
+
+
+##LIMMA  has not been used here therefore the pvalues for GO term enrichment was FAKED by using the rank-based score 
+  # I FAKE PVALUES HERE TO TEST OUT THE WORKFLOW
+#MID
+mid_erminej <- top20_mid_enriched %>%
+  mutate(
+    rank = row_number(desc(mid_score)),
+    fake_p = rank^2 / (n() + 1)^2 ##basicially taking the rank devided by 21 (out of my top20 table) and square it so that it looks like pvalues
+  ) %>%
+  select(Genes, fake_p)
+
+write.table(
+  mid_erminej,
+  file = "mid_erminej_input.txt",
+  sep = "\t",
+  row.names = FALSE,
+  quote = FALSE
+)
+#HIND
+hind_erminej <- top20_hind_enriched %>%
+  mutate(
+    rank = row_number(desc(hind_score)),
+    fake_p = rank^2 / (n() + 1)^2 
+  ) %>%
+  select(Genes, fake_p)
+
+write.table(
+  hind_erminej,
+  file = "hind_erminej_input.txt",
+  sep = "\t",
+  row.names = FALSE,
+  quote = FALSE
+)
+
+#CROP
+crop_erminej <- top20_crop_enriched %>%
+  mutate(
+    rank = row_number(desc(crop_score)),
+    fake_p = rank^2 / (n() + 1)^2
+  ) %>%
+  select(Genes, fake_p)
+
+write.table(
+  crop_erminej,
+  file = "crop_erminej_input.txt",
+  sep = "\t",
+  row.names = FALSE,
+  quote = FALSE
+)
+
+View(crop_erminej)
+head(pg_final$Genes)
+
+
+## eDIT YOUR 
+library(dplyr)
+library(readr)
+library(stringr)
+
+anno <- read_tsv(file.choose())
+
+erminej_anno <- anno %>%
+  transmute(
+    probe_id = Entry,
+    gene_symbol = Entry,
+    gene_name = Entry,
+    go_ids = str_replace_all(`Gene Ontology IDs`, ";\\s*", "|")
+  ) %>%
+  filter(!is.na(go_ids), go_ids != "")
+
+write.table(
+  erminej_anno,
+  file = "erminej_annotation_final.txt",
+  sep = "\t",
+  row.names = FALSE,
+  col.names = TRUE,
+  quote = FALSE
+)
+sum(erminej_anno$probe_id %in% pg_final$Protein.Group) #This should be >>0 otherwise it wouldnt run on ErmineJ
+View(erminej_anno)
